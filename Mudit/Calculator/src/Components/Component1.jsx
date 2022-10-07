4e8c8a47a97025eb15e050915385ea62c49f2108
MuditArya3 <Mudit.Arya@bhavnacorp.com>
2022-10-07 11:50:51 +0530

    Commit

---
+++ Mudit/Calculator/src/Components/Component1.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+export class Hello extends Component{
+
+    render(){
+        return (
+          <div className='heading'>
+          <h1>CASIO</h1>
+        </div>      
+          )
+
+    }
+
+}
